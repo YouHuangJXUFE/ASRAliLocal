50c62da8a2f26cd49ba190260bc5c7037263944a
Ouyang Huan <849652207@qq.com>
2020-09-02 09:58:02 +0800

    发送get请求的代码

--- AliLocalAsr/src/main/java/com/asr/FileTransRESTfulDemo.java
+++ AliLocalAsr/src/main/java/com/asr/FileTransRESTfulDemo.java
@@ -23,7 +23,8 @@ public class FileTransRESTfulDemo {
     private static final String KEY_HEADER = "header";
     private static final String KEY_PAYLOAD = "payload";
     private static final String KEY_STATUS_MESSAGE = "status_message";
-    private static final String KEY_TASK_ID = "task_id"; private static final String STATUS_SUCCESS = "SUCCESS";
+    private static final String KEY_TASK_ID = "task_id";
+    private static final String STATUS_SUCCESS = "SUCCESS";
     private static final String STATUS_SUCCESS_WITH_NO_VALID_FRAGMENT = "SUCCE SS_WITH_NO_VALID_FRAGMENT";
     private static final String STATUS_RUNNING = "RUNNING";
     private static final String STATUS_QUEUEING = "QUEUEING";
@@ -88,7 +89,62 @@ public class FileTransRESTfulDemo {
         }
 
         return taskId;
+    }
 
+    public String getFileTransResult(String url, String taskId) {
+        /**
+         * 设置HTTP GET请求
+         * 1.使用HTTP协议
+         * 2.录音文件识别服务域名：您的gateway的域名： ip:port
+         * 3.录音文件识别请求路径： /stream/v1/filetrans
+         * 4.设置必须请求参数：task_id
+         */
+        url = url + "?task_id=" + taskId;
+        System.out.println("url: " + url);
 
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .build();
+
+        String statusMessage = null;
+        String result = null;
+        OkHttpClient client = new OkHttpClient();
+        while (true) {
+            try {
+                /**
+                 * 发送HTTP GET请求，处理服务端返回的响应
+                 */
+                Response response = client.newCall(request).execute();
+                int responseCode = response.code();
+                String responseBody = response.body().string();
+                System.out.println("识别查询结果： "+ responseBody);
+                response.close();
+
+                if (200 != responseCode) {
+                    break;
+                }
+
+                JSONObject rootObj = JSONObject.parseObject(responseBody);
+                // 从header中获取状态信息
+                JSONObject headerObj = rootObj.getJSONObject(KEY_HEADER);
+                statusMessage = headerObj.getString(KEY_STATUS_MESSAGE);
+                if (STATUS_RUNNING.equals(statusMessage) || STATUS_QUEUEING.equals(statusMessage)) {
+                    // 继续轮询，注意设置轮询时间间隔
+                    Thread.sleep(3000);
+                }
+                else {
+                    // 状态信息返回成功，返回识别结果；状态信息为异常则返回空
+                    if (STATUS_SUCCESS.equals(statusMessage) || STATUS_SUCCESS_WITH_NO_VALID_FRAGMENT.equals(statusMessage)) {
+                        result = rootObj.getJSONObject(KEY_PAYLOAD).toString();
+                    }
+                    break;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        return result;
     }
 }
